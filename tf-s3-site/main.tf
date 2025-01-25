328de69306618853ec65c993ea7025fd221d9f5c
sarathmohan95 <sarathkumar.mohan6@gmail.com>
2025-01-25 19:29:52 -0400

    updating tf resource

--- tf-s3-site/main.tf
+++ tf-s3-site/main.tf
@@ -10,13 +10,16 @@ resource "aws_s3_bucket_website_configuration" "static_website" {
   }
 }
 
-resource "aws_s3_bucket_object" "index_html" {
+resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket_website_configuration.static_website.bucket
   key    = "index.html"
   source = "path/to/your/local/index.html" 
   acl    = "public-read"
 }
-
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket_website_configuration.bucket_acl.id
+  acl    = "public-read"
+}
 output "website_url" {
   value = aws_s3_bucket_website_configuration.static_website.website_endpoint
 }
