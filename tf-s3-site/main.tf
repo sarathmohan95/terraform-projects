d49a2b08ffe561d4dbf7d6112cec388854b70af4
sarathmohan95 <sarathkumar.mohan6@gmail.com>
2025-01-25 19:34:25 -0400

    updating tf resource

--- tf-s3-site/main.tf
+++ tf-s3-site/main.tf
@@ -16,10 +16,6 @@ resource "aws_s3_object" "index_html" {
   source = "path/to/your/local/index.html" 
   acl    = "public-read"
 }
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket_website_configuration.bucket_acl.id
-  acl    = "public-read"
-}
 output "website_url" {
   value = aws_s3_bucket_website_configuration.static_website.website_endpoint
 }
