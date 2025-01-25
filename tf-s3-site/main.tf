c6c182e4d8479abb937a8bf7f70e3d57732cae1a
sarathmohan.devops <sarathkumar.mohan6@gmail.com>
2025-01-25 19:43:38 -0400

    Update main.tf

--- tf-s3-site/main.tf
+++ tf-s3-site/main.tf
@@ -6,16 +6,24 @@ resource "aws_s3_bucket_website_configuration" "static_website" {
   bucket = "msarathkumar-site"
 
   index_document {
-    suffix = "./src/index.html"
+    suffix = "index.html"
   }
 }
 
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket_website_configuration.static_website.bucket
   key    = "index.html"
-  source = "path/to/your/local/index.html" 
+  source = "./src/index.html" 
   acl    = "public-read"
 }
+
+resource "aws_s3_object" "supporting_files" {
+  bucket = aws_s3_bucket_website_configuration.static_website.bucket
+  key    = "img/picture.JPEG"
+  source = "./src/img/picture.JPEG" 
+  acl    = "public-read"
+}
+
 output "website_url" {
   value = aws_s3_bucket_website_configuration.static_website.website_endpoint
 }
