122972e70d32f68eb877b0dbcab38f8cf3d5e6d2
sarathmohan.devops <sarathkumar.mohan6@gmail.com>
2025-04-16 17:32:10 -0300

    adding acm cert

---
+++ tf-acm-cert/main.tf
@@ -0,0 +1,5 @@
+module "s3_site" {
+  source = "git::https://github.com/sarathmohan95/terraform-modules.git//acm/public?ref=v0.0.3"
+  validation_method = "DNS"
+  domain_name = "sarathmohan.work"
+}
