f864837a04f3e4ab4cd11805fc2aa64241338993
sarathmohan.devops <sarathkumar.mohan6@gmail.com>
2025-04-16 21:13:23 -0300

    Update main.tf

--- tf-acm-cert/main.tf
+++ tf-acm-cert/main.tf
@@ -1,4 +1,4 @@
-module "s3_site" {
+module "s3_site_1" {
   source = "git::https://github.com/sarathmohan95/terraform-modules.git//acm/public?ref=v0.0.3"
   validation_method = "DNS"
   domain_name = "sarathmohan.work"
